61e2152753b9e40d76e083357d4f900030c665fd
lindolo25 <lindolo25@outlook.com>
2018-09-23 17:54:17 -0400

    completed the functionallity and did some ajustment on the styles and views

--- js/trivia.js
+++ js/trivia.js
@@ -37,16 +37,17 @@ var trivia = {
         // check is there is any question remaining if not break ---
         if(this.currentQuestion >= this.questions.length)
         {
+            this.computeResult();
             return;
         }        
 
         // print the questions on screen ------------------
         var triviaOptions = $("#trivia-options");
-        triviaOptions.html('<div class="col-12"><p id="description">'+ this.questions[this.currentQuestion].description +'</p></div>');
+        triviaOptions.html('<div id="question-head" class="col-12"><p>'+ this.questions[this.currentQuestion].description +'</p></div>');
 
         for(i = 0; i < this.questions[this.currentQuestion].answers.length; i++)
         {
-            var temp = $('<div class="col-12 col-sm-6 col-lg-4"><p class="option" data-index="'+ i +'">'+ this.questions[this.currentQuestion].answers[i] +'</p></div>');
+            var temp = $('<div class="col-12 col-sm-6"><p class="option" data-index="'+ i +'">'+ this.questions[this.currentQuestion].answers[i] +'</p></div>');
             temp.children("p").on("click", function() { 
                 var selected = $(this).attr("data-index");
                 trivia.completeQuestion(selected); 
@@ -83,22 +84,64 @@ var trivia = {
     {
         console.log("question completed");
         clearInterval(this.timeCounter);
-        this.computeResult(selected);
-        this.startTimer(10, function() { trivia.popQuestion() }, true);
+        this.computeQuestion(selected);
+        this.startTimer(5, function() { trivia.popQuestion() }, false);
     },
     timeOut: function()
     {
         console.log("time's Out");
-        this.computeResult(-1);
-        this.startTimer(10, function() { trivia.popQuestion() }, true);
+        this.computeQuestion(-1);
+        this.startTimer(5, function() { trivia.popQuestion() }, false);
     },
-    computeResult: function(selected)
+    computeQuestion: function(selected)
     {
+        selected = parseInt(selected);
+        var title = "";
         console.log("computer selected: "+ selected);
-        if()
+        if(selected === -1)
+        {
+            title = "<h4 class=\"neutral\">Time's Up!</h4>";
+            this.unansweredQuestions++;
+        }
+        else if(this.questions[this.currentQuestion].correctAnswer === selected)
+        {
+            title = "<h4 class=\"correct\">Correct !!!</h4>";
+            this.correctAnswers++;
+        }
+        else
+        {
+            title = "<h4 class=\"incorrect\">Incorrect!</h4>";
+            $(".option[data-index=\""+ selected +"\"]").removeClass("option").addClass("option-incorrect").off("click");
+            this.incorretAnswers++;
+        }
+        $(".option[data-index=\""+ this.questions[this.currentQuestion].correctAnswer +"\"]").removeClass("option").addClass("option-correct").off("click");
+        $(".option").removeClass("option").addClass("option-neutral").off("click");
+        $("#question-head").prepend(title);
+    },
+    computeResult: function()
+    {
+        var result = $('<div class="col-12 col-sm-8 col-md-6">\
+                <p>All done! This is how you did.</p>\
+                <div><p class="option-neutral relative">Correct Answers: <span class="badge badge-success">'+ this.correctAnswers +'</span></p></div>\
+                <div><p class="option-neutral relative">Incorrect Answers: <span class="badge badge-danger">'+ this.incorretAnswers +'</span></p></div>\
+                <div><p class="option-neutral relative">Unswered Questions: <span class="badge badge-dark">'+ this.unansweredQuestions +'</span></p></div>\
+            </div>\
+            <div class="col-12">\
+                <button id="restart-trivia" class="btn btn-secondary">Restart.</button>\
+            </div>')
+            
+        result.find("#restart-trivia").on("click", function() { trivia.restart(); });
+        $("#trivia-options").html(result);
     },
     restart: function()
     {
+        this.correctAnswers = 0;
+        this.incorretAnswers = 0;
+        this.unansweredQuestions = 0;
+        this.currentQuestion = -1;
+        this.timeCounter = undefined;
+        this.interval = 0;
 
+        this.popQuestion();
     }
 }
